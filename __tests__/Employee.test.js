1c58ac2f1a922c9ed707379bf1295299f55812b3
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-30 17:55:27 -0500

    Created first failing test for Employee object

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Andrew', 1, 'ahudg@ahudg.com');
+
+    expect(employee.name).toBe('Andrew');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+})
