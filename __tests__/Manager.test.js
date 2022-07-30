d01b978d924ffcc04ebbcc4634a0813e6e356e1e
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-30 18:13:41 -0500

    Created the Manager object and passing test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,7 @@
+const Manager = require('../lib/Manager');
+
+test('creates the manager constructor', () => {
+    const manager = new Manager('Andrew', 1);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
