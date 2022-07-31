3a09a75beb962655d93b12c2fca6fd6a83554036
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-31 11:44:34 -0500

    Added getName() test to Employee Obstructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,3 +6,8 @@ test('creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 })
+
+test('checks if getName returns object name', () => {
+    const employee = new Employee('Andrew', 1, 'ahudg@ahudg.com');
+    expect(employee.getName()).toBe('Andrew');
+})
