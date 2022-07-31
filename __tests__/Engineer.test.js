bf0b9e571fa4ce0b2e1ed6ba04c3b1af348f009e
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-31 12:30:44 -0500

    Created tests and methods for Engineer object

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,21 @@
 const Engineer = require('../lib/Engineer');
 
-test('creates an engineer object', () => {
-    const engineer = new Engineer('Andrew', 1, 'ahudg@ahudg.com', 'AHudg');
-    expect(engineer.name).toBe('Andrew');
-    expect(engineer.id).toEqual(expect.any(Number));
-    expect(engineer.email).toEqual(expect.any(String));
-    expect(engineer.github).toEqual(expect.any(String));
+describe('tests the creation and methods of Engineer', () => {
+    test('creates an engineer object', () => {
+        const engineer = new Engineer('Andrew', 1, 'ahudg@ahudg.com', 'AHudg');
+        expect(engineer.name).toBe('Andrew');
+        expect(engineer.id).toEqual(expect.any(Number));
+        expect(engineer.email).toEqual(expect.any(String));
+        expect(engineer.github).toEqual(expect.any(String));
+    });
+
+    test('checks that getRole returns Engineer', () => {
+        const engineer = new Engineer('Andrew', 1, 'ahudg@ahudg.com', 'AHudg');
+        expect(engineer.getRole()).toBe('Engineer');
+    })
+
+    test('checks that getGithub returns the objects github', () => {
+        const engineer = new Engineer('Andrew', 1, 'ahudg@ahudg.com', 'AHudg');
+        expect(engineer.getGithub()).toEqual('AHudg');
+    })
 });
