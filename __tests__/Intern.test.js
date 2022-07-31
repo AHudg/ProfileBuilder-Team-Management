9bf43b1598254fa3c7d05895d841902a2af1cb85
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-31 12:34:42 -0500

    Created methods and tests for Intern object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,21 @@
 const Intern = require('../lib/Intern');
 
-test('creates an intern object', () => {
-    const intern = new Intern('Andrew', 1, 'ahudg@ahudg.com', 'school');
-    expect(intern.name).toBe('Andrew');
-    expect(intern.id).toEqual(expect.any(Number));
-    expect(intern.email).toEqual(expect.any(String));
-    expect(intern.school).toEqual(expect.any(String));
+describe('tests the creation and methods of Intern', () => {
+    test('creates an intern object', () => {
+        const intern = new Intern('Andrew', 1, 'ahudg@ahudg.com', 'school');
+        expect(intern.name).toBe('Andrew');
+        expect(intern.id).toEqual(expect.any(Number));
+        expect(intern.email).toEqual(expect.any(String));
+        expect(intern.school).toEqual(expect.any(String));
+    });
+
+    test('checks that getRole returns Intern', () => {
+        const intern = new Intern('Andrew', 1, 'ahudg@ahudg.com', 'school');
+        expect(intern.getRole()).toBe('Intern');
+    })
+
+    test('checks that getSchool returns the objects school', () => {
+        const intern = new Intern('Andrew', 1, 'ahudg@ahudg.com', 'school');
+        expect(intern.getSchool()).toBe(intern.school);
+    })
 });
