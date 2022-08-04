88c94fbaf2d8b76be60102fa361444f5f02ffb94
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-03 22:49:25 -0500

    Added README.md and commented code

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const printRosterHtml = (data) => { {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/teamRoster.html', data, err => {
+            // if the write file could not be written for some reason, throw error
             if (err) {
                 reject(err);
                 return;
